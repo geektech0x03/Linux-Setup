e2456747d62644b18b968fc3d1c7723cae292a11
Geek Tech <45627103+geektech0x03@users.noreply.github.com>
2024-10-10 18:27:24 +0530

    Update KL-Setup.sh

--- KL-Setup.sh
+++ KL-Setup.sh
@@ -60,7 +60,7 @@ echo -e "${Cyan}----- Installing Dependencies -----${NC}"
 
 sudo apt-get update
 sudo apt-get install python3.12 python3.12-venv -y  # SD --> GDB Plugins
-sudo apt-get install git ghidra checksec ropper alacarte aptitude patchelf gobuster gdb google-android-platform-tools-installer -y
+sudo apt-get install git ghidra checksec ropper alacarte aptitude patchelf gobuster gdb google-android-platform-tools-installer gcc-multilib -y
 sudo pip install pwntools
 
 clear
