c78e30a9811798f942ed84d78543827806e6b091
Geek Tech <45627103+geektech0x03@users.noreply.github.com>
2024-10-22 09:33:09 -0400

    Update KL-Setup.sh

--- KL-Setup.sh
+++ KL-Setup.sh
@@ -199,7 +199,7 @@ clear
 echo -e "${Cyan}----- Bytecode Viewer ----${NC}"
 
 cd Software/BCViewer
-wget "https://objects.githubusercontent.com/github-production-release-asset-2e65be/25201361/4572c427-6733-4dfa-8fb9-ecfa7a69884e?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20241019%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241019T052957Z&X-Amz-Expires=300&X-Amz-Signature=074c7bdf628d94b26814b4ae6d546d55b210db5d8cea97a09cc5f42e10c914a8&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3DBytecode-Viewer-${bver}.jar&response-content-type=application%2Foctet-stream"
+wget "https://github.com/Konloch/bytecode-viewer/releases/download/v${bver}/Bytecode-Viewer-${bver}.jar"
 mv ./Bytecode-Viewer-${bver}.jar ./bcv.jar
 
 cd ~
